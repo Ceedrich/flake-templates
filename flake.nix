516b671d8c880f35419ca9d29dd92443187f9e31
Cedric Lehr <info@ceedri.ch>
2026-04-08 16:40:02 +0200

    add description for rust

--- flake.nix
+++ flake.nix
@@ -10,6 +10,16 @@
       rust = {
         path = ./rust;
         description = "Rust template";
+        welcomeText = ''
+          # Simple Rust template
+          Edit the `name` to match your project's name and add `buildInputs` and `nativeBuildInputs` if needed.
+
+          ## Usage
+          Develop normally with `cargo`. Nix builds are cached using [naersk](github.com/nix-community/naersk).
+
+          ## Getting started
+          Run `cargo init` and commit your files to start programming 🦀.
+        '';
       };
     };
   };
